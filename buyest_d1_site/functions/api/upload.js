86647580f29375c8fd227775997cf721e355a7ac
buyestkorea-code <buyestkorea@gmail.com>
2026-04-08 05:54:13 +0900

    Delete buyest_d1_site directory

--- buyest_d1_site/functions/api/upload.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return new Response("UPLOAD_OK", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8"
-    }
-  });
-}
